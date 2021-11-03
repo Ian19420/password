6a76cbdf0b530b4d5bf58b8e7ba6689d7c10ef0b
Ian <glps1004@gmail.com>
2021-11-03 20:54:59 +0800

    new file

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'a123456'
+ans = input('請輸入密碼:')
+i = 1
+t = 2
+while i < 3:
+	if ans != 'a123456':
+		ans = input(f'請再試一次,你還剩{t}次機會:')
+		t -= 1
+		i += 1
+		continue
+	else:
+		print('登入成功!')
+print('你沒機會了')
